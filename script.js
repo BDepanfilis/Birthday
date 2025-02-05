12fc3aed0b828f2a7004185b5e13b61a10a49e8c
BDepanfilis <124092128+BDepanfilis@users.noreply.github.com>
2025-02-05 13:48:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ let cakes = [
   { name: "Chocolate Tiramisu Cake", elo: 1000, img: "images/Chocolate_Tiramisu.JPG" },
   { name: "Thai Dream Cake", elo: 1000, img: "images/Thai_Dream.JPG" },
   { name: "Lemon Merengue Cake", elo: 1000, img: "images/Lemon_Merengue.JPG" },
-  { name: "Red Velvet Cake", elo: 1000, img: "images/Red_Velvet2.jpg" },
-  { name: "Double Chocolate Mocha Cake", elo: 1000, img: "images/Double_Chocolate_Mocha.jpg" },
-  { name: "Tuxedo Cake", elo: 1000, img: "images/Tuxedo.jpg" },
-  { name: "Funfetti Cake", elo: 1000, img: "images/Funfetti.jpg" },
-  { name: "Chocolate Kahlua Cake", elo: 1000, img: "images/Chocolate_K.jpg" },
-  { name: "Strawberry Shortcake", elo: 1000, img: "images/Strawberry_Shortcake.jpg" },
-  { name: "Birthday Cake", elo: 1000, img: "images/Birthday_Cake.jpg" },
+  { name: "Red Velvet Cake", elo: 1000, img: "images/Red_Velvet2.JPG" },
+  { name: "Double Chocolate Mocha Cake", elo: 1000, img: "images/Double_Chocolate_Mocha.JPG" },
+  { name: "Tuxedo Cake", elo: 1000, img: "images/Tuxedo.JPG" },
+  { name: "Funfetti Cake", elo: 1000, img: "images/Funfetti.JPG" },
+  { name: "Chocolate Kahlua Cake", elo: 1000, img: "images/Chocolate_K.JPG" },
+  { name: "Strawberry Shortcake", elo: 1000, img: "images/Strawberry_Shortcake.JPG" },
+  { name: "Birthday Cake", elo: 1000, img: "images/Birthday_Cake.JPG" },
   // Add more cakes as needed
 ];
 
@@ -31,17 +31,16 @@ function getRandomCakes() {
   }
 
   // Update the HTML with the correct image paths
-  document.getElementById("cake1").innerHTML = `<img src="images/${cake1.img}" alt="${cake1.name}" />`;
-  document.getElementById("cake2").innerHTML = `<img src="images/${cake2.img}" alt="${cake2.name}" />`;
-
+  document.getElementById("cake1").innerHTML = `<img src="${cake1.img}" alt="${cake1.name}" />`;
+  document.getElementById("cake2").innerHTML = `<img src="${cake2.img}" alt="${cake2.name}" />`;
 
   // Add click events to choose cakes
   document.getElementById("cake1").onclick = function() { updateElo(cake1, cake2); };
   document.getElementById("cake2").onclick = function() { updateElo(cake2, cake1); };
 }
 
+// Elo rating system logic
 function updateElo(winner, loser) {
-  // Elo rating system logic
   const K = 32; // Constant (you can change this to fine-tune)
   let expectedWinner = 1 / (1 + Math.pow(10, (loser.elo - winner.elo) / 400));
   let expectedLoser = 1 - expectedWinner;
@@ -56,16 +55,4 @@ function updateElo(winner, loser) {
 
 // Start with random cakes
 getRandomCakes();
-
-function checkFinalWinner() {
-  // Get the cake with the highest Elo value
-  let highestEloCake = cakes.reduce((prev, current) => (prev.elo > current.elo) ? prev : current);
-
-  // Update the HTML to show the final winner and their image
-  document.getElementById("cake-container").innerHTML = `
-    <h2>Final Winner: ${highestEloCake.name}</h2>
-    <img src="images/${highestEloCake.img}" alt="${highestEloCake.name}" />
-    <p>Happy Birthday, my love!</p>
-  `;
-}
 
